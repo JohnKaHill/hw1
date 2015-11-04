84581bc52d404a6538f69377ad60c57be783b55e
JohnKaHill <John.Kallenberg@hhu.de>
2015-11-04 09:55:32 +0100

    Update fibonacci.cxx

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -1,1 +1,25 @@
+#include <iostream>
+
+int main(){
+    int N;
+    std::cout << "N = ";
+    std::cin >> N;
+    int fib;
+    int a = 0;
+    int b = 1;
+    
+    if (N==1)
+        fib=1;
+    
+    for (int i=1; i<N; ++i) {
+        fib = a + b;
+        a = b;
+        b = fib;
+    }
+    
+    std::cout << "the fibonacci number you are looking for is ";
+    std::cout << fib << std::endl;
+    
+    return 0;
+}
 
